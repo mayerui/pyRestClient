42eaa1c324b1bfa230c0b4ad359a22135d29a6b7
mohuang <mayerui@live.com>
2018-09-29 09:25:50 +0800

    增加切换工作目录的操作
    防止找不到文件

--- changefilename.py
+++ changefilename.py
@@ -15,10 +15,11 @@ newtime=time.strftime("%Y%m%d-%H:%M:%S.000", time.localtime(time.time()))
 header=header.replace(oldtime, newtime, 1)
 lines[0]=header
 
-file = open(path, 'w', newline='\n')
+file = open(path, 'w', newline = '\n')
 file.writelines(lines)
 file.close()
 
+os.chdir(workspace)
 fileList=os.listdir(workspace)
 for fileName in fileList:
     pat = 'cpxx*'
@@ -32,4 +33,22 @@ for fileName in fileList:
     pattern = re.findall(pat, fileName)
     if len(pattern) != 0:
         os.rename(fileName, pattern[0] + newtime[0:8]+'.txt')
+        break
+
+workspace += '/se_cdr'
+os.chdir(workspace)
+fileList = os.listdir(workspace)
+
+for fileName in fileList:
+    pat = 'se018cdr*'
+    pattern = re.findall(pat, fileName)
+    if len(pattern) != 0:
+        os.rename(fileName, pattern[0] + newtime[0:8]+'001.txt')
+        break
+        
+for fileName in fileList:
+    pat = 'se053pqccbglj*'
+    pattern = re.findall(pat, fileName)
+    if len(pattern) != 0:
+        os.rename(fileName, pattern[0] + newtime[0:8]+'001.txt')
         break
