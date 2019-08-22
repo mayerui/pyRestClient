ec26d2741430a26d84bdb1fb17097f992f8146e2
mayerui <mayerui@live.com>
2019-08-22 16:36:14 +0800

    bugfix: 删去多余send

--- pyTCP.py
+++ pyTCP.py
@@ -24,8 +24,6 @@ while 1:
     try:
         for i in range(0, len(dataList)):
             s.send(dataList[i])    
-        s.send(dataList[i])    
-            s.send(dataList[i])    
             time.sleep(1)
             data = s.recv(1024)
             print(bytes(data))
